aa40169ecc36d6e6e43996c1c3f23cf27d392b52
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-06-01 16:50:07 +0100

    Says 'leap year' if passed a year divisible by 4 but not by 100

--- LeapYears/lib/leapYears.rb
+++ LeapYears/lib/leapYears.rb
@@ -1,6 +1,7 @@
 class LeapYears
   def self.check(year)
     return "#{year} is a leap year" if year % 400 == 0
+    return "#{year} is a leap year" if year % 4 == 0 && year % 100 != 0
     "#{year} is not a leap year"
   end
 end
