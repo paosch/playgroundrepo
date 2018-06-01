9449394c00a0ebb0f0c5cf679232ed49ef860310
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-06-01 16:52:35 +0100

    Refactored to eliminate repetition (DRY)

--- LeapYears/lib/leapYears.rb
+++ LeapYears/lib/leapYears.rb
@@ -1,7 +1,9 @@
 class LeapYears
   def self.check(year)
-    return "#{year} is a leap year" if year % 400 == 0
-    return "#{year} is a leap year" if year % 4 == 0 && year % 100 != 0
-    "#{year} is not a leap year"
+    if year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
+      "#{year} is a leap year"
+    else
+      "#{year} is not a leap year"
+    end
   end
 end
