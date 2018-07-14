361b2ece8496656d7a53c42ae26f6c9fcc3c5474
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-07-14 19:25:48 +0100

    Divisibility calculations now made by a separate method

--- fizzbuzz/lib/fizzbuzz.rb
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -1,10 +1,14 @@
 def fizzbuzz(number)
   if number.is_a? Integer
-    return 'Fizzbuzz' if number % 3 == 0 && number % 5 == 0
-    return 'Fizz' if number % 3 == 0
-    return 'Buzz' if number % 5 == 0
+    return 'Fizzbuzz' if is_divisible_by?(number, 3) && is_divisible_by?(number, 5)
+    return 'Fizz' if is_divisible_by?(number, 3)
+    return 'Buzz' if is_divisible_by?(number, 5)
     number
   else
     'I only work with integers!'
   end
+end
+
+def is_divisible_by?(number, divisor)
+  number % divisor == 0
 end
