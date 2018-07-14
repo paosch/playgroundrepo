5c3cf7b56ed1bc59fc4fbe878f2b04af818fa5a2
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-07-14 19:04:45 +0100

    Returns the number passed when it's a multiple neither of 3, nor 5, nor 15

--- fizzbuzz/lib/fizzbuzz.rb
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -2,4 +2,5 @@ def fizzbuzz(number)
   return 'Fizzbuzz' if number % 3 == 0 && number % 5 == 0
   return 'Fizz' if number % 3 == 0
   return 'Buzz' if number % 5 == 0
+  number
 end
--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -9,4 +9,7 @@ describe 'fizzbuzz' do
   it "returns 'Fizzbuzz' when passed a multiple of 15" do
     expect(fizzbuzz(30)).to eq 'Fizzbuzz'
   end
+  it "returns the number passed when it's a multiple neither of 3, nor 5, nor 15" do
+    expect(fizzbuzz(7)).to eq 7
+  end
 end
